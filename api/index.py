4e320670084ee852580768bb4219ffb116c8b4f4
Angrewz <97853812+Angrewz@users.noreply.github.com>
2024-07-16 22:11:07 +0000

    vercel6

--- api/index.py
+++ api/index.py
@@ -11,6 +11,7 @@ async def webhook(request: Request):
     logging.info("Webhook received")
     update = await request.json()
     logging.info(f"Update: {update}")
+    await bot.initialize()  # Инициализируем объект Application перед использованием
     await bot.process_update(update)
     return ''
 
--- main.py
+++ main.py
@@ -22,15 +22,16 @@ async def handle_question(update: Update, context: CallbackContext) -> None:
     title = "Оплата за ответ"
     description = "Ответ в обработке. Закончу сразу после оплаты."
     payload = "Custom-Payload"
-    currency = "XTR"  # Используйте XTR для "Звёзд"
-    price = 1  # Установите цену в минимальных единицах "Звёзд" (1 Звезда = 100 единиц)
+    currency = "XTR"
+    price = 1
     prices = [LabeledPrice("Оплата ответа на важнейший вопрос", price)]
 
-    # Отправка инвойса пользователю
     await context.bot.send_invoice(chat_id, title, description, payload, PAYMENT_PROVIDER_TOKEN, currency, prices)
+    logging.info("Invoice sent")
 
 # Обработчик предварительной проверки оплаты
 async def precheckout_callback(update: Update, context: CallbackContext) -> None:
+    logging.info("Precheckout received")
     query = update.pre_checkout_query
     if query.invoice_payload != 'Custom-Payload':
         await query.answer(ok=False, error_message="Что-то пошло не так...")
@@ -39,6 +40,7 @@ async def precheckout_callback(update: Update, context: CallbackContext) -> None
 
 # Обработчик успешной оплаты
 async def successful_payment_callback(update: Update, context: CallbackContext) -> None:
+    logging.info("Successful payment received")
     question = context.user_data.get('question', '')
     response = f"{question} - хороший вопрос, но я не знаю на него ответ. Зато желаю тебе всех благ. Добро возвращается! Смайлик"
     await update.message.reply_text(response)
@@ -48,11 +50,10 @@ async def set_webhook(application: Application):
     logging.info(f"Setting webhook to: {webhook_url}")
     await application.bot.set_webhook(webhook_url)
 
-def main() -> Application:
+def create_application() -> Application:
     logging.info("Starting bot")
     application = Application.builder().token(BOT_TOKEN).build()
 
-    # Регистрация обработчиков
     application.add_handler(CommandHandler('start', start))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_question))
     application.add_handler(PreCheckoutQueryHandler(precheckout_callback))
@@ -60,5 +61,6 @@ def main() -> Application:
 
     return application
 
-bot = main()
+# Создаем приложение и устанавливаем вебхук
+bot = create_application()
 asyncio.run(set_webhook(bot))
